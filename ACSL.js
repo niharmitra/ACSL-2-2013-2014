799355a4186ab7993a14b820e1cdc88ec3c679e9
niharmitra <Nihar@niharmitrasimac.hsd1.ca.comcast.net>
2014-02-08 23:02:34 -0800

    Started basic framework

--- ACSL.js
+++ ACSL.js
@@ -3,6 +3,26 @@ var submit_button = document.getElementById("submit_button");
 var input;
 
 submit_button.onclick = function() {
+	input = readCommand();
+	if(input.search("E")!=-1) {
+
+	}
+}
+
+function readCommand() {
 	input = input_box.value;
-	window.alert(input);
+	//splits input into array of two elements
+	input = input.split(", ");
+	input[1] = parseFloat(input[1]);
+	return input;
+}
+
+//takes input and rounds accordingly
+function round(input) {
+	//finds amount of space available according to string
+	var space = input[0].match(/&/g).length;
+	//finds the number of digits in the value
+	var digits = input[1].toString().length;
+
+	Math.round(input[1]*)
 }
