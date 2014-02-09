5b4d9a8d1d842ab6f8e8f52e25a42c47acc8206c
niharmitra <Nihar@niharmitrasimac.hsd1.ca.comcast.net>
2014-02-09 09:07:40 -0800

    Working on round and fill functions

--- ACSL.js
+++ ACSL.js
@@ -1,6 +1,6 @@
 var input_box = document.getElementById("input_box");
 var submit_button = document.getElementById("submit_button");
-var input;
+var input; //makes sure input is a global variable
 
 submit_button.onclick = function() {
 	input = readCommand();
@@ -17,12 +17,18 @@ function readCommand() {
 	return input;
 }
 
-//takes input and rounds accordingly
+/*takes input and rounds accordingly
+Only called on decimal values*/
 function round(input) {
-	//finds amount of space available according to string
+	//finds amount of space available according to string (number of &'s)
 	var space = input[0].match(/&/g).length;
-	//finds the number of digits in the value
-	var digits = input[1].toString().length;
+	//used to allow usage of Math.round(integer)
+	var round_factor = Math.pow(10, space);
+	return Math.round(input[1]*round_factor)/round_factor;
+}
 
-	Math.round(input[1]*)
+//takes input and fills the *'s as needed
+function fill(input) {
+	//finds the number of digits in the value
+	var digits = input[1].toString().length-1; //minus 1 because of .
 }
