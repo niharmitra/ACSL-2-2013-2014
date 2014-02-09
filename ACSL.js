049897e16b8ed30b7224b5f3080b83f423cf7e17
niharmitra <Nihar@niharmitrasimac.hsd1.ca.comcast.net>
2014-02-09 12:16:47 -0800

    Working on algorithms

--- ACSL.js
+++ ACSL.js
@@ -13,38 +13,57 @@ submit_button.onclick = function() {
 		//removes the *$ once processed
 		input[0] = input[0].slice(1);
 	}
+	else {
+		output = process(input);
+		displayOutput(output);
+	}
 }
 
 function readCommand() {
 	input = input_box.value;
+	if(input == null) {
+		window.alert("Please enter valid input");
+	}
 	//splits input into array of two elements
 	input = input.split(", ");
 	input[1] = parseFloat(input[1]);
 	return input;
 }
 
-/*takes input and rounds accordingly
-Only called on decimal values*/
-function round(input_param) {
-	//finds amount of space available according to string (number of &'s)
-	var space = input_param[0].match(/&/g).length;
-	//used to allow usage of Math.round(integer)
-	var round_factor = Math.pow(10, space-2);
-	console.log(space);
-	console.log(round_factor);
-	return Math.round(input_param[1]*round_factor)/round_factor;
-}
+//takes input and fills the *'s, 0's or rounds as needed
+function process(input_param) {
+	var string = input_param[0].split(".");
+	//space is amount of space in front of decimal
+	var space = string[0].match(/&/g).length;
 
-//takes input and fills the *'s as needed
-function fill(input_param) {
-	
-}
-
-function decimal(input_param) {
-	//finds amount of space available according to string (number of &'s)
-	var space = input_param[0].match(/&/g).length;
 	//finds the number of digits in the value
 	var digits = input_param[1].toString().length-1; //minus 1 because of .
+
+	//if there isn't a decimal, directly fill with *'s
+	if(string.length<2) {
+		console.log("Processing non-decimal");
+		digits += 1; //add back the 1 subtracted above (assumed decimal)
+		console.log(digits);
+		console.log(space);
+		output = input_param[1];
+		for(var i;i<=space-digits;i++) {
+			output = "*"+output;
+			console.log(output);
+		}
+		return output;
+	}
+	
+	//dec_space is amount of space after decimal point
+	var dec_space = string[1].match(/&/g).length;
+
+
+	if(space<digits) {
+		var round_factor = Math.pow(10, digits);
+		return Math.round(input_param[1]*round_factor)/round_factor;
+	}
+	else {
+
+	}
 }
 
 function exponentialForm(input_param) {
