43c2beb1ab9ec038e0b2f87412733812b2d7a14c
niharmitra <Nihar@niharmitrasimac.hsd1.ca.comcast.net>
2014-02-08 20:15:51 -0800

    Editing CSS

--- ACSL.js
+++ ACSL.js
@@ -1,4 +1,8 @@
 var input_box = document.getElementById("input_box");
 var submit_button = document.getElementById("submit_button");
+var input;
 
-window.alert("Hi!");
+submit_button.onclick = function() {
+	input = input_box.value;
+	
+}
