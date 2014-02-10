b0bce68a3f7cde782edde4ef1915f8643eb1763f
niharmitra <Nihar@niharmitrasimac.hsd1.ca.comcast.net>
2014-02-09 22:01:33 -0800

    Updating algorithms

--- ACSL.js
+++ ACSL.js
@@ -7,16 +7,30 @@ submit_button.onclick = function() {
 	input = readCommand();
 	if(input[0].search("E")!=-1) {
 		output = exponentialForm(input);
-		displayOutput(output);
 	}
-	else if(input[0].search(/\*$/)!=-1){
+	else if(input[0].indexOf('*$')!=-1) {
 		//removes the *$ once processed
+		input[0] = input[0].slice(2);
+		output = process(input);
+		output_array = new Array();
+		output_array[0] = output.substring(0,output.lastIndexOf("*"));
+		output_array[1] = output.slice(output.lastIndexOf("*"));
+		console.log(output_array);
+		output = output_array[0] + "$" + output_array[1];
+	}
+	else if(input[0].indexOf("$") != -1)  {
 		input[0] = input[0].slice(1);
+		output = process(input);
+		output = "$"+output;
+	}
+	else if(input[0].search(",") != -1) {
+		output = comma(process(input));
 	}
 	else {
 		output = process(input);
-		displayOutput(output);
 	}
+	displayOutput(output);
+
 }
 
 function readCommand() {
@@ -26,6 +40,7 @@ function readCommand() {
 	}
 	//splits input into array of two elements
 	input = input.split(", ");
+	input[0] = input[0].toString();
 	input[1] = parseFloat(input[1]);
 	return input;
 }
@@ -40,30 +55,55 @@ function process(input_param) {
 	var digits = input_param[1].toString().length-1; //minus 1 because of .
 
 	//if there isn't a decimal, directly fill with *'s
-	if(string.length<2) {
-		console.log("Processing non-decimal");
+	if(string.length<=1) {
 		digits += 1; //add back the 1 subtracted above (assumed decimal)
-		console.log(digits);
-		console.log(space);
 		output = input_param[1];
 		for(var i=1;i<=(space-digits);i++) {
 			output = "*"+output;
-			console.log(output);
 		}
-		return output;
-	}
-	
-	//dec_space is amount of space after decimal point
-	var dec_space = string[1].match(/&/g).length;
-
-
-	if(space<digits) {
-		var round_factor = Math.pow(10, digits);
-		return Math.round(input_param[1]*round_factor)/round_factor;
+		return output.toString();
 	}
 	else {
-
+		//dec_space is amount of space after decimal point
+		var dec_space = string[1].match(/&/g).length;
+		var dec_digits = input_param[1].split(".");
+		dec_digits = dec_digits[1].length;
 	}
+	output = input_param[1];
+	//does rounding when needed
+	if(dec_space<dec_digits) {
+		var round_factor = Math.pow(10, dec_digits);
+		output = Math.round(input_param[1]*round_factor)/round_factor;
+	}
+	for(var i=1;i<=(space-digits+dec_digits);i++) {
+		output = "*"+output;
+	}
+	for(var j=1; j<=dec_space-dec_digits;j++) {
+		output += "0";
+	}
+	return output.toString();
+}
+
+function comma(output_param) {
+	var num = Math.floor(output_param.length/3);
+	var output_array = output_param.toString().split("");
+	console.log(output_array);
+	for(var i=1; i<num; i++) {
+		if(i==1){
+			var index = -3;
+		}
+		else {
+			var index = output_param.toString().indexOf(",");
+		}
+		output_array.splice(index, 0, ",");
+		//= output_param.toString().slice(0,index-1)+","+output_param.toString().slice(index);
+	}
+	output = "";
+	console.log(output_array);
+	for(var j=0; j<=output_array.length;j++) {
+		output += output_array[j];
+	}
+	return output;
 }
 
 function exponentialForm(input_param) {
